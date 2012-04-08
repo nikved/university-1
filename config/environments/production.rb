d61c026b0255c7dfbae0d803b5e8dc30ba2c3188
ivan yurchenko <vanya.yu@gmail.com>
2012-04-08 23:45:37 +0300

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ University::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.precompile += ['active_admin.css', 'active_admin/print.css', 'active_admin.js']
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
