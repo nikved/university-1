06680248f91b165334ed490cc9341ec0bb5e6f07
ivan yurchenko <vanya.yu@gmail.com>
2012-04-08 23:32:21 +0300

    mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ University::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+  config.action_mailer.default_url_options = { :host => 'universityproject.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'mail.tut.by',
+    :port                 => 2525,
+    :domain               => 'herokuapp.com',
+    :user_name            => 'university2012',
+    :password             => 'mrMwI6XmsiDpqfZrnrkH',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
 end
